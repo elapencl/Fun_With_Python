9946eec0b31bc9463738dce23e924c12c2bd14ef
Ela Pencl <56656517+elapencl@users.noreply.github.com>
2021-06-16 14:59:32 -0500

    Create bmi_calculator.py

---
+++ bmi_calculator/bmi_calculator.py
@@ -0,0 +1,9 @@
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+new_weight = int(weight)
+new_height = float(height)
+
+print(round(new_weight/(new_height ** 2)))
+
+print(round(2.6378,2))
