73e3e9232a6820db2f9f2bb527b269bab89e25a7
Ela Pencl <56656517+elapencl@users.noreply.github.com>
2021-06-16 15:22:14 -0500

    Create highest_score.py

---
+++ highest_score/highest_score.py
@@ -0,0 +1,11 @@
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+highest = 0
+for i in student_scores:
+  if i > highest:
+    highest = i
+
+print(highest)
