387e9ba850faca7edafe80a99e1fcfffcd642ff1
Ela Pencl <56656517+elapencl@users.noreply.github.com>
2021-06-16 15:00:59 -0500

    Create your_life_in_weeks.py

---
+++ your_life_in_weeks/your_life_in_weeks.py
@@ -0,0 +1,7 @@
+age = input("What is your current age? ")
+
+new_age = 90 - int(age)
+days = new_age * 365
+weeks = new_age * 52
+months = new_age * 12
+print(f"You have {days} days, {weeks} weeks, and {months} months left until the age of 90.")
